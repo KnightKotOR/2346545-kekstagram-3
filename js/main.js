98ffcdcfd531ba3e9dd86d07ea00980ad1157b9a
KKotOR <kirill2000kiril02@mail.ru>
2023-03-16 02:22:04 +0300

    init module2-task1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function randInt(min, max){ // Результат: целое число из диапазона "от...до"
+
+  //Проверка удовлетворению условий
+  if (min < 0 || max < 0 || min > max){
+    console.log ('Некорректно заданный диапозон');
+  }
+
+  //Получение и округления результата
+  else{
+    return Math.floor(Math.random * (max - min + 1) + min);
+  }
+}
+
+function lengthCheck(str, maxLength){ // Результат: true, если строка проходит по длине, и false — если не проходит
+  if (str.length > maxLength){
+    return true;
+  }
+  return false;
+}
