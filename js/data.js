b9df7e7bede8f1a9d3b05daf5156384c719bf292
KKotOR <kirill2000kiril02@mail.ru>
2023-03-17 02:11:15 +0300

    init module5-task1

---
+++ js/data.js
@@ -0,0 +1,19 @@
+import { randInt } from "./util.js";
+
+const generatePhoto = (n) => ({
+  id: n,
+  url: `photos/${  n  }.jpg`,
+  description: `jpg № ${ n }`,
+  likes: randInt(15, 200),
+  comments: randInt(0, 200),
+})
+
+export const generatePhotos = (n) => {
+  const data = [];
+  for (let i = 0; i < n; i++) {
+    data[i] = photo(i+1);
+  }
+  return data;
+}
+
+photoData(25);
