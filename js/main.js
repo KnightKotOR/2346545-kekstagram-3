11801030e2c4624d944e2fc6c90f6f3549290cd9
KKotOR <kirill2000kiril02@mail.ru>
2023-05-09 18:22:54 +0300

    module9-init

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import { generatePhotos } from './data.js';
 import { insertMin } from './picMin.js';
 import './form.js';
 import './formValidator.js';
+import './scale.js';
+import './effect.js';
 
 const pictures = generatePhotos(25);
 insertMin(pictures);
